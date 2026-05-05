6739a9ff0ec35cc9f617515db4056cef1db91b96
khe4oyan <khe4oyan@gmail.com>
2026-05-05 10:43:56 +0400

    [feat] show last 3 updates & button for more

--- src/components/Updates/Updates.jsx
+++ src/components/Updates/Updates.jsx
@@ -11,22 +11,27 @@ import updates from "../../constants/updates";
 import classes from "./styles.module.css";
 
 export default function Updates() {
-  const [isShowOldUpdates, setIsShowUpdates] = useState(true);
+  const [isShowOldUpdates, setIsShowUpdates] = useState(false);
   const { versions } = updates;
 
-  const [lastUpdate, ...otherUpdates] = versions;
+  const alwaysShowingUpdates = versions.slice(0, 3);
+  const hiddenUpdates = versions.slice(3, versions.length);
 
   return (
     <div className={classes.root}>
       <div className={classes.versions}>
-        <Update data={lastUpdate} />
+        {
+          alwaysShowingUpdates.map((version, ind) => 
+            <Update data={version} key={ind} />
+          )
+        }
 
         {isShowOldUpdates ? (
-          otherUpdates.map((version, ind) => (
+          hiddenUpdates.map((version, ind) => (
             <Update key={ind} data={version} />
           ))
         ) : (
-          <button className={classes.button} onClick={() => setIsShowUpdates(true)}>show prev updates</button>
+          <button className={classes.button} onClick={() => setIsShowUpdates(true)}>Տեսնել ավելի հին թարմացումները</button>
         )}
       </div>
     </div>
