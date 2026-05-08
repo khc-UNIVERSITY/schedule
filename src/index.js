5736bb970bb4762841ab271874dcd088ab2cb002
khe4oyan <khe4oyan@gmail.com>
2026-05-08 11:49:38 +0400

    [feat] redux init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
+// libs
 import ReactDOM from "react-dom/client";
-import App from "./App";
 import { HashRouter } from "react-router-dom";
+import { Provider } from 'react-redux'
+
+// components
+import App from "./App";
 import AllComponentsPreview from './components/AllComponentsPreview';
+
+// store
+import store from "./store/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <HashRouter>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </HashRouter>
 );
 
---
+++ src/store/store.js
@@ -0,0 +1,10 @@
+// libs
+import { configureStore } from "@reduxjs/toolkit";
+
+// slices
+
+const store = configureStore({
+  reducer: {},
+});
+
+export default store;
