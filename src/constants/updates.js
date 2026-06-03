1db89dd852b0d8f4f255b59ced2d1a2cd653f50e
khe4oyan <khe4oyan@gmail.com>
2026-06-03 22:44:05 +0400

    [chore] update version

--- src/constants/updates.js
+++ src/constants/updates.js
@@ -19,7 +19,7 @@ import artemis2Img from '../assets/img/updates/theme-artemis_ii.png';
 import modeImg from '../assets/img/updates/mode.png';
 import eventsImg from '../assets/img/updates/events.png';
 
-const lastUpdateAt = "2026-06-03T08:22:23.720Z"
+const lastUpdateAt = "2026-06-03T18:44:05.459Z"
 
 export const listTypes = {
   text: "text",
